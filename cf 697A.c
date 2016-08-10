3a5eb727ed76e3cdbfaa548aac8ef11bc371a6c2
shawon.ice.nstu <shuvroshawon9@gmail.com>
2016-08-10 15:13:40 +0600

    Add files via upload

---
+++ cf 697A.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    int t,s,x;
+    scanf("%d %d %d",&t,&s,&x);
+    if(t>x) printf("NO\n");
+    else if(t==x) printf("YES\n");
+    else{
+        int i=1,check=0,m=t,j=0;
+        for(;t<=x;i++){
+            if(i%2!=0){
+                t=m+((++j)*s);
+            }
+            else t+=1;
+            if(t==x){
+                check=1;
+                break;
+            }
+        }
+        if(check) printf("YES\n");
+        else printf("NO\n");
+    }
+    return 0;
+}
