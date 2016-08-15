2243c17b02af395d20caae07c9d5718a33c36853
ShawonNSTU <shuvroshawon9@gmail.com>
2016-08-15 12:04:30 +0600

    Add files via upload

---
+++ cf 158A.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int ara[51];
+    int i,n,k;
+    scanf("%d %d",&n,&k);
+    for(i=1;i<=n;i++) scanf("%d",&ara[i]);
+    int num=ara[k];
+    int count=0;
+    for(i=1;i<=n;i++){
+        if(ara[i]>=num&&ara[i]>0) count++;
+    }
+    printf("%d\n",count);
+}
