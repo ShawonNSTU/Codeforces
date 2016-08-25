0dfa46b3cf9c05d38774c841defbfed82d47520e
ShawonNSTU <shuvroshawon9@gmail.com>
2016-08-25 08:34:02 +0600

    Add files via upload

---
+++ cf 709A.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+long long int oranges[100001];
+int main()
+{
+    int waste=0,i,n,b,d;
+    scanf("%d %d %d",&n,&b,&d);
+    for(i=0;i<n;i++)
+        scanf("%d",&oranges[i]);
+    long long int sum=0;
+    for(i=0;i<n;i++){
+        if(oranges[i]<=b){
+            sum+=oranges[i];
+            if(sum>d){
+                waste++;
+                sum=0;
+            }
+        }
+    }
+    printf("%d\n",waste);
+    return 0;
+}
