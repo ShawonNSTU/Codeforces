9eb1c17ed09501663a437efe7d4afc846c599cdf
ShawonNSTU <shuvroshawon9@gmail.com>
2016-09-10 04:43:24 +0600

    Add files via upload

---
+++ cf 699A.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    int x,n;
+    string str;
+    vector<int>v;
+    scanf("%d",&n);
+    cin>>str;
+    for(int i=1;i<=n;i++){
+        scanf("%d",&x);
+        v.push_back(x);
+    }
+    bool check=false;
+    int sec=2e9;
+    printf("%d\n",sec);
+    for(int i=0;i<str.size();i++){
+       if(str[i]=='R'&&str[i+1]=='L'){
+        check=true;
+        sec=min((v[i+1]-v[i])/2,sec);
+       }
+    }
+    if(check==false) printf("-1\n");
+    else printf("%d\n",sec);
+    return 0;
+}
